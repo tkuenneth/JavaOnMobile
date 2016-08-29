813af81eb4d1ffeb17e22f82abc71732ac9e8439
Thomas Künneth <thomas@thomaskuenneth.com>
2016-08-29 18:49:14 +0200

    Added implementation

--- NativeAbstractionDemo/src/android/java/com/thomaskuenneth/gluon/nativeabstractiondemo/AndroidNativeService.java
+++ NativeAbstractionDemo/src/android/java/com/thomaskuenneth/gluon/nativeabstractiondemo/AndroidNativeService.java
@@ -1,9 +1,11 @@
 package com.thomaskuenneth.gluon.nativeabstractiondemo;
 
+import android.widget.Toast;
 import com.gluonhq.charm.down.common.Platform;
 import com.gluonhq.charm.down.common.PlatformFactory;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafxports.android.FXActivity;
 
 public class AndroidNativeService implements NativeService {
 
@@ -18,6 +20,9 @@ public class AndroidNativeService implements NativeService {
 
     @Override
     public void notify(String s) {
+        FXActivity.getInstance().runOnUiThread(() -> {
+            Toast.makeText(FXActivity.getInstance(), s, Toast.LENGTH_LONG).show();
+        });
     }
 
 }
