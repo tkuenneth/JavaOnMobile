70952d0f1388ba9a72e99b282cc8e95e786757f4
Thomas Künneth <thomas@thomaskuenneth.com>
2016-11-04 09:56:53 +0100

    A little bit of refactoring and implented continuous position changes

--- FootSteps/src/main/java/com/thomaskuenneth/gluon/footsteps/FootSteps.java
+++ FootSteps/src/main/java/com/thomaskuenneth/gluon/footsteps/FootSteps.java
@@ -1,8 +1,5 @@
 package com.thomaskuenneth.gluon.footsteps;
 
-import com.gluonhq.charm.down.Services;
-import com.gluonhq.charm.down.plugins.Position;
-import com.gluonhq.charm.down.plugins.PositionService;
 import com.gluonhq.charm.glisten.application.MobileApplication;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
@@ -13,32 +10,32 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class FootSteps extends MobileApplication {
-    
+
     private static final Logger LOGGER = Logger.getLogger(FootSteps.class.getName());
     private static final ResourceBundle BUNDLE = getResourceBundle();
-    private static final Position NUREMBERG = new Position(49.45, 11.08);
-    
+
     private MainView mainView;
-    
+
     @Override
     public void init() {
         mainView = new MainView(HOME_VIEW);
         addViewFactory(HOME_VIEW, () -> mainView);
     }
-    
+
     @Override
     public void postInit(Scene scene) {
         ((Stage) scene.getWindow()).getIcons().add(new Image(FootSteps.class.getResourceAsStream("/footsteps.png")));
-        mainView.setPosition(getPosition());
+        mainView.setMapCenterToCurrentPosition();
+        mainView.startReceivingPositionChanges();
     }
-    
+
     public static String getString(String key) {
         if (BUNDLE.containsKey(key)) {
             return BUNDLE.getString(key);
         }
         return "";
     }
-    
+
     private static ResourceBundle getResourceBundle() {
         ResourceBundle bundle = null;
         try {
@@ -47,19 +44,5 @@ public class FootSteps extends MobileApplication {
             LOGGER.log(Level.SEVERE, "bundle not found", e);
         }
         return bundle;
-    }
-    
-    private Position getPosition() {
-        return Services.get(PositionService.class)
-                .map(positionService -> {
-                    Position pos = positionService.getPosition();
-                    if (pos == null) {
-                        pos = NUREMBERG;
-                    }
-                    return pos;
-                })
-                .orElseGet(() -> {
-                    return NUREMBERG;
-                });
     }
 }
--- FootSteps/src/main/java/com/thomaskuenneth/gluon/footsteps/MainView.java
+++ FootSteps/src/main/java/com/thomaskuenneth/gluon/footsteps/MainView.java
@@ -1,14 +1,20 @@
 package com.thomaskuenneth.gluon.footsteps;
 
+import com.gluonhq.charm.down.Services;
 import com.gluonhq.charm.down.plugins.Position;
+import com.gluonhq.charm.down.plugins.PositionService;
 import com.gluonhq.charm.glisten.control.AppBar;
 import com.gluonhq.charm.glisten.mvc.View;
 import com.gluonhq.maps.MapPoint;
 
 import com.gluonhq.maps.MapView;
+import javafx.application.Platform;
+import javafx.beans.property.ReadOnlyObjectProperty;
 import javafx.scene.input.MouseButton;
 
 public class MainView extends View {
+
+    private static final Position NUREMBERG = new Position(49.45, 11.08);
 
     private final MapView mapView;
     private final FootStepsLayer layer;
@@ -30,13 +36,55 @@ public class MainView extends View {
         });
     }
 
-    public void setPosition(Position pos) {
-        MapPoint start = new MapPoint(pos.getLatitude(), pos.getLongitude());
-        mapView.setCenter(start);
+    /**
+     * Sets the center of the map to the current position.
+     */
+    public void setMapCenterToCurrentPosition() {
+        Platform.runLater(() -> {
+            Position pos = getCurrentPosition();
+            MapPoint mapPoint = new MapPoint(pos.getLatitude(), pos.getLongitude());
+            mapView.setCenter(mapPoint);
+        });
+    }
+
+    /**
+     * Start receiving position changes, if a position service si available-
+     */
+    public void startReceivingPositionChanges() {
+        Services.get(PositionService.class).ifPresent((service) -> {
+            ReadOnlyObjectProperty<Position> positionProperty = service.positionProperty();
+            positionProperty.addListener(e -> {
+                Position pos = positionProperty.get();
+                if (pos != null) {
+                    mapView.setCenter(pos.getLatitude(), pos.getLongitude());
+                }
+            });
+        });
     }
 
     @Override
     protected void updateAppBar(AppBar appBar) {
         appBar.setTitleText(FootSteps.getString("title"));
     }
+
+    /**
+     * Gets the current position. If no position service is available, a
+     * hardcoded position is returned.
+     *
+     * @return current position
+     */
+    private Position getCurrentPosition() {
+        return Services.get(PositionService.class)
+                .map(positionService -> {
+                    Position pos = positionService.getPosition();
+                    if (pos == null) {
+                        pos = NUREMBERG;
+                    }
+                    return pos;
+                })
+                .orElseGet(() -> {
+                    return NUREMBERG;
+                });
+    }
+
 }
